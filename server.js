c21ac213743d1c2378377ee555476bd8cb6a7482
jahangxxr <153099080+jahangxxr@users.noreply.github.com>
2024-05-18 01:34:07 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
@@ -6,7 +7,7 @@ const app = express();
 app.use(bodyParser.json());
 
 const db = mysql.createPool({
-  connectionLimit : 10,
+  connectionLimit: 10,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
