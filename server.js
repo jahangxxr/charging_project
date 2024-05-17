4c0a555753de34447d3d73135e731161bffa3ca9
jahangxxr <153099080+jahangxxr@users.noreply.github.com>
2024-05-17 17:51:30 +0500

    Update server.js

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.get('/', (req, res) => {
   res.send('Welcome to the API!');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
