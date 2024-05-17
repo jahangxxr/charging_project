0a7408593d3f6b0a47a40ed380c6143487144ba4
jahangxxr <153099080+jahangxxr@users.noreply.github.com>
2024-05-17 17:01:07 +0500

    Update server.js

--- server.js
+++ server.js
@@ -59,6 +59,11 @@ app.post('/api/updateBilling', (req, res) => {
   });
 });
 
+// Define route handler for GET request to the root URL
+app.get('/', (req, res) => {
+  res.send('Welcome to the API!');
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
